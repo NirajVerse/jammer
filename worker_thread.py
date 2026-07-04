1301347f698b0d9a71ac7e35df45e03871d980c1
Charles Ueltschey <me@cueltschey.com>
2026-07-04 17:55:48 -0500

    Fix worker thread for integration

--- worker_thread.py
+++ worker_thread.py
@@ -1,4 +1,6 @@
 from worker_thread import WorkerThread
+import os
+
 
 class jammer(WorkerThread):
     def start(self):
@@ -6,7 +8,17 @@ class jammer(WorkerThread):
         self.cleanup_old_containers()
         self.setup_env()
         self.setup_networks()
-        self.config.container_volumes[self.config.config_file] = {"bind": "/jammer.yaml", "mode": "ro"}
-        self.setup_volumes()
 
+        self.config.container_volumes[self.config.config_file] = {
+            "bind": "/jammer.yaml", "mode": "ro"
+        }
+
+        config_dir = os.path.dirname(os.path.abspath(self.config.config_file))
+        self.config.container_volumes[config_dir] = {
+            "bind": "/jammer", "mode": "rw"
+        }
+
+        self.config.host_network = True
+
+        self.setup_volumes()
         self.start_container()
